bb8d8f4710d4bca89743364be1322d6a17f1fbc0
KevinBK <kevinbijoykurian@gmail.com>
2022-01-16 19:19:43 +0530

    Reformat Instructions

--- src/main/java/com/kbkapps/playgba/cpu/constants/Instructions.java
+++ src/main/java/com/kbkapps/playgba/cpu/constants/Instructions.java
@@ -1,11 +1,24 @@
 package com.kbkapps.playgba.cpu.constants;
 
 public enum Instructions {
-    B("branch"), CMP("compare"), MOV("move"), MOVS("move-s"),
-    LDR("load"), LDR_PC("load pc-relative"),
-    TEQ("exclusive test"), MRS("move psr to reg"), ORR("logical or"),
-    MSR("move reg to psr"), STR("store"), BL("branch with link"),
-    ADD("add"), BX("exchanging branch");
+    B("branch"),
+    CMP("compare"),
+    MOV("move"),
+    MOVS("move-s"),
+    LDR("load"),
+    LDMIA("load from memory"),
+    LDR_PC("load pc-relative"),
+    TEQ("exclusive test"),
+    MRS("move psr to reg"),
+    ORR("logical or"),
+    MSR("move reg to psr"),
+    STR("store"),
+    STRMIA("store in memory"),
+    BL("branch with link"),
+    ADD("add"),
+    BX("exchanging branch"),
+    PUSH("push"),
+    POP("pop");
 
     private final String message;
 
