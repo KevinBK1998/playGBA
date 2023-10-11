f990dea71485b2a79aba33702e9730b51d3b3a5f
Kevin Bijoy <kevinbijoykurian@gmail.com>
2023-10-11 18:12:11 +0530

    refactor ioreg

--- GPU.cpp
+++ GPU.cpp
@@ -14,18 +14,45 @@ void GPU::storeHalfWord(int address, uint16_t* var, uint8_t data){
 }
 
 void GPU::write8(uint8_t address, uint8_t data){
+    uint16_t* pointer;
+    int actualAddress;
     switch (address & 0xFE)
     {
     case DISPCNT:
-        storeHalfWord(address&1, &dispCnt, data);
+        pointer = &dispCnt;
+        break;
+    case DISPSTAT:
+        pointer = &dispStat;
+        break;
+    case VCOUNT:
+        pointer = &vCount;
+        break;
+    case BGCNT_OFFSET ... BGCNT_END:
+        actualAddress = (address>>1) & 0b11;
+        pointer = &bgCnt[actualAddress];
+        break;
+    case BGHW_OFFSET ... BGHW_OFFSET_END:
+        actualAddress = (address>>1) & 0b111;
+        pointer = (actualAddress&1)? &bgOffsetY[actualAddress>>1]: &bgOffsetX[actualAddress>>1];
+        break;
+    case BG2PAR_OFFSET ... BG2PAR_OFFSET_END:
+        actualAddress = (address>>1) & 0b11;
+        pointer = &bg2Parameters[actualAddress];
         break;
     default:
         cout << "W: GPU Undefined Memory: "<<unsigned(address)<<endl;
         exit(FAILED_DMA);
     }
-    status();
+    storeHalfWord(address&1, pointer, data);
+    if(address&1)
+        status();
 }
 
 void GPU::status(){
-    cout<<"DISPCNT: "<<dispCnt<<endl;
+    cout<<"\nGPU:"<<endl;
+    cout<<"DISPCNT: "<<dispCnt<<"\tDISPSTAT: "<<dispStat<<"\tVCOUNT: "<<vCount<<endl;
+    cout<<"BG0CNT: "<<bgCnt[0]<<"\tBG1CNT: "<<bgCnt[1]<<"\tBG2CNT: "<<bgCnt[2]<<"\tBG3CNT: "<<bgCnt[3]<<endl;
+    cout<<"BG0HOFS: "<<bgOffsetX[0]<<"\tBG0HOFS: "<<bgOffsetX[1]<<"\tBG0HOFS: "<<bgOffsetX[2]<<"\tBG0HOFS: "<<bgOffsetX[3]<<endl;
+    cout<<"BG0VOFS: "<<bgOffsetY[0]<<"\tBG0VOFS: "<<bgOffsetY[1]<<"\tBG0VOFS: "<<bgOffsetY[2]<<"\tBG0VOFS: "<<bgOffsetY[3]<<endl;
+    cout<<"BG2PA: "<<bg2Parameters[0]<<"\tBG2PB: "<<bg2Parameters[1]<<"\tBG2PC: "<<bg2Parameters[2]<<"\tBG2PD: "<<bg2Parameters[3]<<endl<<endl;
 }
--- GPU.h
+++ GPU.h
@@ -5,9 +5,23 @@
 using namespace std;
 
 const int DISPCNT = 0;
+const int DISPSTAT = 4;
+const int VCOUNT = 6;
+const int BGCNT_OFFSET = 8;
+const int BGCNT_END = 0xE;
+const int BGHW_OFFSET = 0x10;
+const int BGHW_OFFSET_END = 0x1E;
+const int BG2PAR_OFFSET = 0x20;
+const int BG2PAR_OFFSET_END = 0x26;
 
 class GPU{
     uint16_t dispCnt;
+    uint16_t dispStat;
+    uint16_t vCount;
+    uint16_t bgCnt[4];
+    uint16_t bgOffsetX[4];
+    uint16_t bgOffsetY[4];
+    uint16_t bg2Parameters[4];
     void storeHalfWord(int, uint16_t*, uint8_t);
 public:
     void write8(uint8_t, uint8_t);
